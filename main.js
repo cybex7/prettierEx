20343f07e615c6a2e24e1f5534e6bb72230fe0ee
arash <arash.abdous@gmail.com>
2023-04-05 11:09:31 +0330

    add tasks https://javascript.info/comparison

--- main.js
+++ main.js
@@ -6,5 +6,11 @@ document.addEventListener("DOMContentLoaded", function () {
     let fiveth = undefined === null;
     let sixth = null == "\n0\n";
     let seventh = null === +"\n0\n";
-    console.log(seventh);
+    document.getElementById("first").innerHTML = first;
+    document.getElementById("second").innerHTML = second;
+    document.getElementById("third").innerHTML = third;
+    document.getElementById("fourth").innerHTML = fourth;
+    document.getElementById("fiveth").innerHTML = fiveth;
+    document.getElementById("sixth").innerHTML = sixth;
+    document.getElementById("seventh").innerHTML = seventh;
 });
